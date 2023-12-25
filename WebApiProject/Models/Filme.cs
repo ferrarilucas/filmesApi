8c53249b10e41844bb17064a0c359299c173e229
ferrarilucas <ferrari.lucasr@gmail.com>
2023-12-25 20:56:09 -0300

    Final Project Commit

---
+++ WebApiProject/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiProject.Models;
+
+public class Filme
+{
+    [Required]
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage ="O titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+    
+    [Required(ErrorMessage ="O Genero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage ="O tamanho do genero não pode exceder 50 Caracteres")]
+    public string Genero { get; set; }
+    
+    [Required(ErrorMessage ="A duração do filme é obrigatório")]
+    [Range(70, 600, ErrorMessage =" A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
